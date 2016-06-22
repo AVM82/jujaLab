96532845f479b503ee453b85c32a8d8dfcd371e9
AVM82 <1982AlexVM@gmail.com>
2016-06-22 15:38:35 +0300

    Lab 36 is DONE !

---
+++ src/com/AVM/lab28/AbstractShip.java
@@ -0,0 +1,32 @@
+package com.AVM.lab28;
+
+/**
+ * Created by AVM on 29.04.2016.
+ */
+abstract class AbstractShip {
+    private String name;
+    private float length;
+    private float width;
+    private float displacement;
+
+    public AbstractShip(String name, float length, float width, float displacement) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+        this.displacement = displacement;
+    }
+
+    public abstract float calculatePayment();
+    //public abstract float calculatePayment(float arg);
+
+    public String toPrint() {
+        return "Name=" + name +
+                "Length=" + length +
+                "Width=" + width +
+                "Displacement=" + displacement;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
